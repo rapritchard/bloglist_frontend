4bc9a629c640b72fa8964cd67fa42186740a38b1
Reece Pritchard <rapritchard96@gmail.com>
2019-11-20 19:32:15 +0000

    Added login functionality and added user details to localstorage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import loginService from './services/login';
+import blogService from './services/blogs';
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((initialBlogs) => {
+      setBlogs(initialBlogs);
+    });
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username, password,
+      });
+
+      window.localStorage.setItem(
+        'loggedBloglistUser', JSON.stringify(user),
+      );
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('Failed to Login');
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h2> Login </h2>
+      <div>
+        <input
+          type="text"
+          value={username}
+          name="username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          name="password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to the blog list</h2>
+        {loginForm()}
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blogs</h1>
+      <p>
+        {user.name} logged in
+      </p>
+      <div>
+        {blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
